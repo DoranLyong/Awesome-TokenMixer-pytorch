ec51b64dc7445ba4bbaa86ac2e6a95a8bb109d15
DoranLyong <cheeryun@gmail.com>
2022-11-01 12:09:37 +0900

    Update External_Attention.py

--- model/attention/External_Attention.py
+++ model/attention/External_Attention.py
@@ -1,2 +1,4 @@
 # (ref) 3rd: https://github.com/xmu-xiaoma666/External-Attention-pytorch/blob/master/model/attention/ExternalAttention.py
 # (ref) official: https://github.com/MenghaoGuo/EANet/blob/main/model_torch.py
+# (ref) JSeg: https://github.com/Jittor/JSeg/blob/main/python/jseg/ops/external_attention.py
+# (ref) Blog: https://sjpyo.tistory.com/96
