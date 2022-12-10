dc9f9be928f44116a2f84a600132d02f250af23f
DoranLyong <doranlyong@gmail.com>
2022-12-10 18:54:02 +0900

    GFNet

--- model/spectral/globalfilter_GFNet.py
+++ model/spectral/globalfilter_GFNet.py
@@ -1,15 +1,16 @@
-""" Code reference:
-    - https://github.com/raoyongming/GFNet
-"""
 import math 
 
 import torch 
 import torch.nn as nn 
-from torch.cuda.amp import autocast
 
+from timm.models.layers import trunc_normal_
 
 
 class GlobalFilter(nn.Module):
+    """ Original implementation refer to (GFNet, NeurIPS 2021):
+        - https://github.com/raoyongming/GFNet
+        - https://gfnet.ivg-research.xyz/
+    """
     def __init__(self, dim, h=14, w=8):
         super().__init__()
         self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02) # (h, w, dim, Re/Im)
@@ -36,11 +37,50 @@ class GlobalFilter(nn.Module):
 
 
 
+class FourierUnit(nn.Module):
+    """ Custom implementation
+    """
+    def __init__(self, dim, h=14, w=8):
+        super().__init__()
+        self.complex_weight = nn.Parameter(torch.randn(dim, h, w, 2, dtype=torch.float32) * 0.02) # (C, H, W, real+imag)
+        trunc_normal_(self.complex_weight, std=.02) # init complex weight
+
+    def forward(self, x):
+        assert x.ndim == 4
+        bias = x.clone()
+        B,C,H,W = x.size() 
+        
+        # == FFT == # 
+        x = x.to(torch.float32)
+        x = torch.fft.rfft2(x, dim=(2, 3), norm='ortho') # (B, C, H, W//2+1)
+
+        # == Spectral transform == # 
+        weight = torch.view_as_complex(self.complex_weight).type(torch.complex64) # ((B, C, H, W//2+1); torch.complex64 for preventing half-precision error of cuFFT 
+        x = x * weight
+
+        # == Inverse FFT == #
+        x = torch.fft.irfft2(x, s=(H, W), dim=(2, 3), norm='ortho') # (B, C, H, W)
+        return x + bias 
+
+
+
 if __name__ == "__main__":
+
+    # == Original implementation == #
     dim = 64 
     H, W = (14, 14)
     x = torch.randn(1, H*W, dim)  # (batch_size, seq_len, hidden_size)
 
-    block = GlobalFilter(dim, h=H, w=(W//2+1))
+    block = GlobalFilter(dim, h=H, w=(W//2+1)) # 
     out = block(x)
     print(out.shape)
+
+
+    # == Custom implementation == #
+    x2 = torch.randn(1, dim, H, W)  # (batch_size, hidden_size, H, W)
+
+    block2 = FourierUnit(dim, h=H, w=(W//2+1))
+    out2 = block2(x2)
+    print(out2.shape)
+
+
