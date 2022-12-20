a97979114103a123b420f9e0ed89ca223aabc879
DoranLyong <doranlyong@gmail.com>
2022-12-20 16:49:16 +0900

    batch_norm

---
+++ model/norm/batch_norm2D.py
@@ -0,0 +1,7 @@
+""" Batch normalization (also called "batch scale") is a technique used to normalize the activations of the layers in a deep learning model, 
+    specifically the mean and variance of the activations over a mini-batch of training examples.
+
+    (ref) https://youtu.be/4gal2zIjm3M
+    (ref) https://gaussian37.github.io/dl-concept-batchnorm/
+"""
+
