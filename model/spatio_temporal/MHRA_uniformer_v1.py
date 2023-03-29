dbe979333a18154c2ed1fe87178ef730d7529dc7
DoranLyong <cheeryun@gmail.com>
2023-03-30 08:02:23 +0900

    uniformer_v1

--- model/spatio_temporal/MHRA_uniformer_v1.py
+++ model/spatio_temporal/MHRA_uniformer_v1.py
@@ -1,1 +1,234 @@
-#
+# (ref) https://github.com/Sense-X/UniFormer/blob/main/video_classification/slowfast/models/uniformer.py
+""" Multi-Head Relation Aggregator (MHRA) for video classification.
+"""
+from functools import partial
+
+import torch 
+import torch.nn as nn 
+
+from timm.models.layers import trunc_normal_, DropPath, to_2tuple
+
+def conv_1x1x1(inp, oup, groups=1):
+    return nn.Conv3d(inp, oup, (1, 1, 1), (1, 1, 1), (0, 0, 0), groups=groups)
+
+def conv_3x3x3(inp, oup, groups=1):
+    return nn.Conv3d(inp, oup, (3, 3, 3), (1, 1, 1), (1, 1, 1), groups=groups)
+
+def conv_5x5x5(inp, oup, groups=1):
+    return nn.Conv3d(inp, oup, (5, 5, 5), (1, 1, 1), (2, 2, 2), groups=groups)
+
+def bn_3d(dim):
+    return nn.BatchNorm3d(dim)
+
+# ---------------- #
+# == Local MHRA == # 
+# ---------------- #
+class CMlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = conv_1x1x1(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = conv_1x1x1(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class CBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.pos_embed = conv_3x3x3(dim, dim, groups=dim)
+        self.norm1 = bn_3d(dim)
+        self.conv1 = conv_1x1x1(dim, dim, 1)
+        self.conv2 = conv_1x1x1(dim, dim, 1)
+        self.attn = conv_5x5x5(dim, dim, groups=dim)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = bn_3d(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = CMlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.pos_embed(x)
+        x = x + self.drop_path(self.conv2(self.attn(self.conv1(self.norm1(x)))))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x   
+
+
+# ---------------------------------------- #
+# == Global MHRA (Joint spatiotemporal) == # 
+# ---------------------------------------- #
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class SABlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.pos_embed = conv_3x3x3(dim, dim, groups=dim)
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.pos_embed(x)
+        B, C, T, H, W = x.shape
+        x = x.flatten(2).transpose(1, 2)
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        x = x.transpose(1, 2).reshape(B, C, T, H, W)
+        return x    
+
+# ------------------------------------------ #
+# == Global MHRA (Divided spatiotemporal) == # 
+# ------------------------------------------ #
+class SplitSABlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.pos_embed = conv_3x3x3(dim, dim, groups=dim)
+        self.t_norm = norm_layer(dim)
+        self.t_attn = Attention(
+            dim,
+            num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop)
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+            attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.pos_embed(x)
+        B, C, T, H, W = x.shape
+        attn = x.view(B, C, T, H * W).permute(0, 3, 2, 1).contiguous()
+        attn = attn.view(B * H * W, T, C)
+        attn = attn + self.drop_path(self.t_attn(self.t_norm(attn)))
+        attn = attn.view(B, H * W, T, C).permute(0, 2, 1, 3).contiguous()
+        attn = attn.view(B * T, H * W, C)
+        residual = x.view(B, C, T, H * W).permute(0, 2, 3, 1).contiguous()
+        residual = residual.view(B * T, H * W, C)
+        attn = residual + self.drop_path(self.attn(self.norm1(attn)))
+        attn = attn.view(B, T * H * W, C)
+        out = attn + self.drop_path(self.mlp(self.norm2(attn)))
+        out = out.transpose(1, 2).reshape(B, C, T, H, W)
+        return out
+
+
+if __name__ == "__main__":
+    head_dim=64
+    mlp_ratio=4.
+    qkv_bias=True 
+    qk_scale=None 
+    representation_size=None
+    drop_rate=0.
+    attn_drop_rate=0. 
+    drop_path_rate=0. 
+    norm_layer=partial(nn.LayerNorm, eps=1e-6)
+
+    # --- 
+    depth=[3, 4, 8, 3]
+    embed_dim=[64, 128, 320, 512]
+    num_heads = [dim // head_dim for dim in embed_dim]
+    dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depth))]  # stochastic depth decay rule    
+    
+    num_frames = 8
+
+    # --- local MHRA ---
+    x = torch.randn(1, embed_dim[0], num_frames, 56, 56) # input 
+    localMHRA =  nn.ModuleList([
+            CBlock(dim=embed_dim[0], num_heads=num_heads[0], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                    drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth[0])])
+    
+    for blk in localMHRA:
+        x = blk(x)
+        print(x.shape)
+
+    # --- global MHRA (joint version) ---
+    x = torch.randn(1, embed_dim[2], num_frames, 14, 14) # input 
+    globalMHRA_joint = nn.ModuleList([
+                SABlock(dim=embed_dim[2], num_heads=num_heads[2], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                        drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i+depth[0]+depth[1]], norm_layer=norm_layer)
+                for i in range(depth[2])])
+    
+    for blk in globalMHRA_joint:
+        x = blk(x)
+        print(x.shape)
+
+    # --- global MHRA (split version) ---        
+    x = torch.randn(1, embed_dim[2], num_frames, 14, 14) # input 
+    globalMHRA_split = nn.ModuleList([
+                SplitSABlock(dim=embed_dim[2], num_heads=num_heads[2], mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i+depth[0]+depth[1]], norm_layer=norm_layer)
+                for i in range(depth[2])])
+
+    for blk in globalMHRA_split:
+        x = blk(x)
+        print(x.shape)
+
