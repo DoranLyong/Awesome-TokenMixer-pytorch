9951dcef308e03d3b9322951f4e53568c44899ab
DoranLyong <doranlyong@gmail.com>
2024-01-24 15:15:51 +0900

    StarRelU

--- model/activation/StarReLU.py
+++ model/activation/StarReLU.py
@@ -26,13 +26,18 @@ class StarReLU(nn.Module):
 
 
 if __name__ == "__main__": 
-    x = torch.linspace(-5, 5, 1000)
+    x = torch.linspace(-5, 5, 1000, requires_grad=True) # requires_grad=True로 설정하여 자동 미분 가능)
     act = StarReLU()
     out = act(x)
+
+    out.backward(torch.ones_like(x)) # out의 각 요소에 대해 역전파 수행
+    x_grad = x.grad
     
     # == Vis. == #
     plt.plot(x.detach().numpy(), out.detach().numpy(), c='r', label='StarReLU')
+    plt.plot(x.detach().numpy(), x_grad.detach().numpy(), c='b',label='Gradient of StarReLU')
     plt.xlim(-5, 5)
     plt.ylim(-3, 5)
-    plt.legend(loc='best')
+    plt.legend(fontsize="20", loc='best')
+    plt.grid(True)
     plt.show() 
