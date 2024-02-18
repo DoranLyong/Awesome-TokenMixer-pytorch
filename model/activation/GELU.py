e6775b4f1cedb08b771f30c2371c8b0affbf15f4
DoranLyong <cheeryun@gmail.com>
2024-02-18 14:51:45 +0900

    GELU

--- model/activation/GELU.py
+++ model/activation/GELU.py
@@ -16,13 +16,19 @@ class GELU(nn.Module):
 
 
 if __name__ == "__main__": 
-    x = torch.linspace(-5, 5, 1000)
+    x = torch.linspace(-5, 5, 1000, requires_grad=True) # requires_grad=True로 설정하여 자동 미분 가능
     act = GELU()
     out = act(x)
+    out.backward(torch.ones_like(x)) # out의 각 요소에 대해 역전파 수행
+    x_grad = x.grad 
 
     # == Vis. == #
-    plt.plot(x, out, c='r', label='GELU')
-    plt.xlim(-5, 5)
-    plt.ylim(-3, 5)
-    plt.legend(loc='best')
-    plt.show() 
+    plt.plot(x.detach().numpy(), out.detach().numpy(), c='r', label=f'GELU')
+    plt.plot(x.detach().numpy(), x_grad.detach().numpy(), c='r', linestyle='--' ,label='Gradient of GELU')    
+
+    plt.title("GELU Activation Function")
+    plt.xlabel("Input Value (x)")
+    plt.ylabel("Output Value")
+    plt.legend(fontsize="20", loc='best')
+    plt.grid(True)
+    plt.show()
